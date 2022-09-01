620940e747ff0e4eb13301e6268abd5ee91f9ae2
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-31 22:04:09 -0700

    removed default answers

--- index.js
+++ index.js
@@ -12,25 +12,21 @@ inquirer
       type: 'input',
       name: 'Member_Name',
       message: 'What is the name of the team member?',
-      default: 'James Bond'
       },
     {
       type: 'number',
       name: 'Member_Id',
       message: 'What is the id number of the team member?',
-      default: 007
       },
     {
       type: 'input',
       name: 'Member_Email',
       message: 'What is the Email address of the team member?',
-      default: 'James.bond@MI7.com'
       },
     {
       type: 'input',
       name: 'Member_Office',
       message: 'What is the office number of this manager?',
-      default: 'Room 006',
       when(answers) {
         return answers.Member_Role === 'Manager'
       },
@@ -39,7 +35,6 @@ inquirer
       type: 'input',
       name: 'Member_Github',
       message: 'What is the Github Username of this Engineer?',
-      default: 'Q',
       when(answers) {
         return answers.Member_Role === 'Engineer'
       },
@@ -48,7 +43,6 @@ inquirer
       type: 'input',
       name: 'Member_School',
       message: 'What is the School of this Student?',
-      default: 'University of Washington',
       when(answers) {
         return answers.Member_Role === 'Intern'
      },
