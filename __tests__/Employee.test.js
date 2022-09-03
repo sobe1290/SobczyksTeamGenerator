5aeb7d7c22eab3302808f310d068f1272983d96a
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-02 22:22:58 -0700

    reformatting inquirer questions

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee');
+
+
+describe('Employee Information Test', () {
+    describe('Gathering of employees name', () => {
+        it('should return a string with employees name', () => {
+            const newEmployee = new Employee();
+        })
+    })
+});
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const inquirer = require('inquirer');
 const generateHTML = require('./src/generateHTML');
 
-inquirer
-  .prompt([{
-      type: 'list',
-      name: 'Member_Role',
-      message: 'What is the role of the team member you wish to add?',
-      choices: ['Employee','Manager','Engineer','Intern']
-      },
+
+const managerQuestion = [
+  
     {
       type: 'input',
-      name: 'Member_Name',
+      name: 'Manager_Name',
       message: 'What is the name of the team member?',
       },
     {
@@ -27,29 +23,66 @@ inquirer
       type: 'input',
       name: 'Member_Office',
       message: 'What is the office number of this manager?',
-      when(answers) {
-        return answers.Member_Role === 'Manager'
-      },
     },
-    {
-      type: 'input',
-      name: 'Member_Github',
-      message: 'What is the Github Username of this Engineer?',
-      when(answers) {
-        return answers.Member_Role === 'Engineer'
-      },
+];
+
+const engineerQuestion = [
+  {
+    type: 'input',
+    name: 'Engineer_Name',
+    message: 'What is the name of the team member?',
     },
-    {
-      type: 'input',
-      name: 'Member_School',
-      message: 'What is the School of this Student?',
-      when(answers) {
-        return answers.Member_Role === 'Intern'
-     },
-    }
-    
-    
-    ])
+  {
+    type: 'number',
+    name: 'Member_Id',
+    message: 'What is the id number of the team member?',
+    },
+  {
+    type: 'input',
+    name: 'Member_Email',
+    message: 'What is the Email address of the team member?',
+    },
+  {
+    type: 'input',
+    name: 'Member_Github',
+    message: 'What is the Github Username of this Engineer?',
+  },
+];
+
+const studentQuestion = [
+  {
+    type: 'input',
+    name: 'Intern_Name',
+    message: 'What is the name of the Intern?',
+    },
+  {
+    type: 'number',
+    name: 'Member_Id',
+    message: 'What is the id number of the team member?',
+    },
+  {
+    type: 'input',
+    name: 'Member_Email',
+    message: 'What is the Email address of the team member?',
+    },
+  {
+    type: 'input',
+    name: 'Member_School',
+    message: 'What is the School of this Student?',
+  },
+];
+
+const additionalQuestion = [
+  {
+    type: 'list',
+    name: 'Member_Role',
+    message: 'What is the role of the team member you wish to add?',
+    choices: ['Engineer','Intern', 'None']
+    },
+]
+
+inquirer
+  .prompt()
   .then(answers => {
     generateHTML(answers);    
   });
