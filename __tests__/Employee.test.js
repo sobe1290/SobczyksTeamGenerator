8ac16b5573361aaa89d8578c7397def137a6870c
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-07 20:28:41 -0700

    Employee Test Working

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,15 +1,84 @@
 import Employee from '../lib/Employee.js';
 
 
-describe('Employee Information Test', () => {
-    describe('Gathering of employees name', () => {
-        it('should return a string with employees name', () => {
-            const newEmpName = new Employee();
-            const name = "ManagerName";
+describe('Employee Class Card Test', () => {
+    describe('Employee Class Object Test', () => {
+        it('Should return an object', () => {
+            const name = "Bob";
+            const id = "0001";
+            const email = "bob@gmail.com";
+            const newEmpTest = new Employee(name,id,email);
+            expect(typeof newEmpTest === 'object').toBe(true);        
+        });
+    });
 
-            const genEmpName = newEmpName.getName(name);
+    describe('Employee Name Test', () => {
+        it('Should set the name', () => {
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);      
+            expect(newEmpTest.name).toBe(name);
+        });
+    });
 
-            expect(genEmpName).toEqual("string");
-        })
-    })
+    describe('Employee Id Test', () => {
+        it('Should set the Id', () => {
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);               
+            expect(newEmpTest.id).toBe(id);
+        });
+    });
+
+    describe('Employee Email Test', () => {
+        it('Should set the Email', () => {
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);
+            expect(newEmpTest.email).toBe(email);
+        });
+    });    
+
+    describe('Test of method to get name', () => {
+        it('should return the employees name through the method', () => {            
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);            
+            expect(newEmpTest.getName()).toBe(name);
+        });
+    });
+
+    describe('Test of method to get id number', () => {
+        it('should return the employees id through the method', () => {
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);            
+            expect(newEmpTest.getId()).toBe(id);
+        });
+    });
+
+    describe('Test of method to get email', () => {
+        it('should return the employees email through the method', () => {            
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const newEmpTest = new Employee(name, id, email);            
+            expect(newEmpTest.getEmail()).toBe(email);
+        });
+    });
+    describe('Test of method to get role', () => {
+        it('should return the employees role through the method', () => {            
+            const name = "Bob";
+            const id = "0001";
+            const email = "Bob@gmail.com";
+            const role = "Employee";
+            const newEmpTest = new Employee(name, id, email);            
+            expect(newEmpTest.getRole()).toBe(role);
+        });
+    });
 });
